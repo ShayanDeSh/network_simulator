0b30f5c3f0533d6d9e41c982c3caaa47ce18dc64
shayan <shafaghi1378@gmail.com>
2020-06-12 18:21:51 +0430

    Concurent read and proccess from socket

--- src/server.rs
+++ src/server.rs
@@ -3,10 +3,7 @@ mod udp;
 pub fn start(port: String) {
     println!("Hello, world!");
     let connection = udp::Con::init(port);
-    loop {
-        let (amt, src, buf) = connection.listen();
-        let s = std::str::from_utf8(&buf[..amt])
-            .expect("Something happened while converting from utf8 to string!!");
-        println!("recived {:?}", s);
-    }
+    let (process_handler, listen_handler) = connection.listen();
+    process_handler.join().unwrap();
+    listen_handler.join().unwrap();
 }
--- src/server/udp.rs
+++ src/server/udp.rs
@@ -1,9 +1,13 @@
 use std::net::UdpSocket;
 use std::collections::HashMap;
+use std::sync::mpsc;
+use std::thread;
 
 pub struct Con {
     socket: UdpSocket,
-    rtable: HashMap<String, String>
+    rtable: HashMap<String, String>,
+    tx: mpsc::Sender<(usize, [u8; 2048])>,
+    rx: mpsc::Receiver<(usize, [u8; 2048])>
 }
 
 
@@ -12,17 +16,35 @@ impl Con {
         let socket  = UdpSocket::bind(format!("127.0.0.1:{}", port))
             .expect("Something went wrong while trying to create UDP socket!!");
         let rtable  = HashMap::new();
+        let (tx, rx) = mpsc::channel();
         Con {
             socket,
-            rtable
+            rtable,
+            tx,
+            rx 
         }
     }
 
-    pub fn listen(&self) -> (usize, std::net::SocketAddr, [u8; 2048]) {
-        let mut buf = [0; 2048];
-        let (amt, src) = self.socket.recv_from(&mut buf)
-            .expect("shit happened");
-        (amt, src, buf)
+    pub fn listen(self) -> (thread::JoinHandle<u32>, thread::JoinHandle<u32>) {
+        let soc = self.socket;
+        let tx  = self.tx;
+        let rx  = self.rx;
+        let process_handler = thread::spawn(move || {
+            loop {
+                let (amt, data) = rx.recv().unwrap();
+                let st          = std::str::from_utf8(&data[..amt]);
+                println!("{:?}", st);
+            }
+        });
+        let listen_handler = thread::spawn(move || {
+            loop {
+                let mut buf = [0; 2048];
+                let (amt, src) = soc.recv_from(&mut buf)
+                    .expect("shit happened");
+                tx.send((amt, buf)).unwrap();
+            }
+        });
+        return (process_handler, listen_handler);
     }
 
     pub fn send_discovery(&self) { 
