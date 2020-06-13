9251d0763eb1e0745779e3dfc2f4678da7d17a7e
shayan <shafaghi1378@gmail.com>
2020-06-13 21:27:22 +0430

    send_discovery added

--- src/server/udp.rs
+++ src/server/udp.rs
@@ -3,12 +3,29 @@ use std::collections::HashMap;
 use std::sync::mpsc;
 use std::sync::Mutex;
 use std::thread;
+use std::mem;
 
 const BUFFER_SIZE: usize = 2048;
+const USEFUL_BUFFER_SIZE: usize = BUFFER_SIZE - 16;
+
+pub struct Host {
+    name: String,
+    ipaddr: String,
+    port: u16
+}
 
 pub struct Server {
     socket: UdpSocket,
+    hosts: Vec<Host>,
     rtable: Mutex<HashMap<String, String>>,
+}
+
+pub struct Header {
+    request: String,
+    dest_port: u16,
+    src_port: u16,
+    dest_ip: String,
+    src_ip: String
 }
 
 
@@ -16,9 +33,11 @@ impl Server {
     pub fn init(port: &str, rtable: Mutex<HashMap<String, String>>) -> Server {
         let socket  = UdpSocket::bind(format!("127.0.0.1:{}", port))
             .expect("Something went wrong while trying to create UDP socket!!");
+        let hosts: Vec<Host> = Vec::new();
         Server {
             socket,
-            rtable,
+            hosts,
+            rtable
         }
     }
 
@@ -67,12 +86,55 @@ impl Server {
         return (process_handler, listen_handler);
     }
 
+
+    fn send_discovery(&self, header: Header) {
+        let mut buf: [u8; BUFFER_SIZE] = [0; BUFFER_SIZE]; 
+        copy_str(&mut buf, 0, &header.request);
+        let req_byte = header.request.as_bytes();
+        for (i, byte) in req_byte.iter().enumerate() {
+            buf[i] = *byte;
+        }
+        let dport_bytes = header.dest_port.to_be_bytes();
+        buf[4] = dport_bytes[0];
+        buf[5] = dport_bytes[1];
+        let sport_bytes = header.src_port.to_be_bytes();
+        buf[6] = sport_bytes[0];
+        buf[7] = sport_bytes[1];
+        let dip = header.dest_ip.replace(".", "");
+        let dip = dip.as_bytes();
+        for (i, num) in dip.iter().enumerate() {
+            buf[8 + i] = *num;
+        }
+        let sip = header.src_ip.replace(".", "");
+        let sip = sip.as_bytes();
+        for (i, num) in sip.iter().enumerate() {
+            buf[12 + i] = *num;
+        }
+        let mut remained_buffer: i32 = USEFUL_BUFFER_SIZE as i32;
+        let mut current = 16;
+        for host in &self.hosts {
+            remained_buffer -= mem::size_of::<Host>() as i32;
+            if remained_buffer < 0 {
+                break;
+            }
+            let name_len = host.name.len() as u8;
+            buf[current] = name_len;
+            current += 1;
+        }
+    }
+
     pub fn send(&self , ipaddr: &str, buf: [u8; BUFFER_SIZE]) {
         let socket  = self.socket.try_clone()
             .expect("Could not clone socket for sending");
         socket.send_to(&buf, ipaddr)
             .expect("Could not send");
     }
+
 }
 
-
+fn copy_str(buf: &mut [u8], current: u16, string: &str) {
+    let req_byte = string.as_bytes();
+    for (i, byte) in req_byte.iter().enumerate() {
+        buf[current as usize + i] = *byte;
+    }
+}
