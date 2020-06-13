f6d0c005b35cc6c504fecc1c364d8df8dc8c92d0
shayan <shafaghi1378@gmail.com>
2020-06-13 15:51:08 +0430

    Reading header from data

--- src/server/udp.rs
+++ src/server/udp.rs
@@ -4,6 +4,8 @@ use std::sync::mpsc;
 use std::sync::Mutex;
 use std::thread;
 
+const BUFFER_SIZE: usize = 2048;
+
 pub struct Server {
     socket: UdpSocket,
     rtable: Mutex<HashMap<String, String>>,
@@ -22,18 +24,41 @@ impl Server {
 
     pub fn listen(&self) -> (thread::JoinHandle<u32>, thread::JoinHandle<u32>) {
         let soc = self.socket.try_clone().expect("Could not clone");
-        let (tx, rx): (mpsc::Sender<(usize, [u8; 2048])>,
-        mpsc::Receiver<(usize, [u8; 2048])>) = mpsc::channel();
+        let (tx, rx): (mpsc::Sender<(usize, [u8; BUFFER_SIZE])>,
+        mpsc::Receiver<(usize, [u8; BUFFER_SIZE])>) = mpsc::channel();
         let process_handler = thread::spawn(move || {
             loop {
                 let (amt, data) = rx.recv().unwrap();
-                let request = std::str::from_utf8(&data[..8]);
+                let request = std::str::from_utf8(&data[..4])
+                    .expect("Not parsable request type");
+                let request =  request.trim();
+                let dest_port: u16 = ((data[4] as u16) << 8) + data[5] as u16; 
+                let src_port: u16 = ((data[6] as u16) << 8) + data[7] as u16; 
+                let dest_ip: String = format!("{}.{}.{}.{}",
+                    data[8], data[9], data[10], data[11]);
+                let src_ip: String = format!("{}.{}.{}.{}",
+                    data[12], data[13], data[14], data[15]);
+                match request {
+                    "get" => {
+                        println!("got get :)");
+                    },
+                    "list" => {
+                        println!("got list");
+                    }
+                    _ => {
+                        continue;
+                    }
+                }
                 println!("{:?}", request);
+                println!("{:?}", dest_port);
+                println!("{:?}", src_port);
+                println!("{:?}", dest_ip);
+                println!("{:?}", src_ip);
             }
         });
         let listen_handler = thread::spawn(move || {
             loop {
-                let mut buf = [0; 2048];
+                let mut buf = [32; BUFFER_SIZE];
                 let (amt, src) = soc.recv_from(&mut buf)
                     .expect("shit happened");
                 tx.send((amt, buf)).unwrap();
@@ -42,12 +67,12 @@ impl Server {
         return (process_handler, listen_handler);
     }
 
+    pub fn send(&self , ipaddr: &str, buf: [u8; BUFFER_SIZE]) {
+        let socket  = self.socket.try_clone()
+            .expect("Could not clone socket for sending");
+        socket.send_to(&buf, ipaddr)
+            .expect("Could not send");
+    }
 }
 
-pub fn send(port: &str) {
-    let socket  = UdpSocket::bind(format!("127.0.0.1:{}", port))
-        .expect("");
-    socket.send_to("Bojac Horseman".as_bytes(),"127.0.0.1:8000")
-        .expect("Could not send");
-}
 
